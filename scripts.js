dc10df149ab02ea4aec903e04dd10de0c301c175
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-22 20:25:13 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Bhagwant Singh");
+$("#policyNumber").text("ML23072120");
+$("#startDate").text("23-May-2020");
+$("#endDate").text("22-May-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08AP3000");
+$("#make").text("TATA MOTORS");
+$("#model").text("INDICA");
+$("#vehicalSubType").text("DL (1405 CC)");
+$("#engineNo").text("30576");
+$("#chassis").text("34328");
